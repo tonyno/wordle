1641cac65771974f074849fb896089487ea32d65
Tonda Kmoch <tonda.kmoch@gmail.com>
2022-01-16 11:38:19 +0100

    sharing and new gui

--- src/components/NextWord.tsx
+++ src/components/NextWord.tsx
@@ -1,14 +1,13 @@
+import { Typography } from "@mui/material";
 import { timeToNextWord } from "../lib/timeFunctions";
 import CountDownTimer from "./counter/CountDownTimer";
 
 const NextWord = () => {
   const hoursMinSecs = timeToNextWord();
   return (
-    <div className="flex justify-center mb-1">
-      <p className="text-sm text-gray-500">
-        {"Další slovo za "} <CountDownTimer hoursMinSecs={hoursMinSecs} />
-      </p>
-    </div>
+    <Typography variant="body2" display="block" className="white">
+      {"Další slovo za "} <CountDownTimer hoursMinSecs={hoursMinSecs} />
+    </Typography>
   );
 };
 
