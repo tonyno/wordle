52c09d1eed99d1071d3b6751858a6f0a54c77079
Tonda Kmoch <tonda.kmoch@gmail.com>
2022-01-25 22:01:48 +0100

    automaticke vydavani slov

--- data_prepare/upload_data_to_fb.py
+++ data_prepare/upload_data_to_fb.py
@@ -1,6 +1,7 @@
 import datetime
 import hashlib
 import json
+import random
 import re
 from typing import List
 
@@ -11,7 +12,8 @@ from firebase_admin import credentials, firestore
 def get_data() -> List[str]:
     words_list = []
     all_words = {}
-    f = open('data_prepare/input/final_words.txt', 'r', encoding='utf-8')
+    f = open('data_prepare/input/final_words_cleaned.txt',
+             'r', encoding='utf-8')
     for line in f:
         word = line.strip().lower()
         if word not in all_words and len(word) == 5 and re.fullmatch('[a-zěščřžýáíéů]+', word):
@@ -20,6 +22,13 @@ def get_data() -> List[str]:
             words_list.append(word)
     f.close()
     return words_list
+
+
+# https://stackoverflow.com/questions/67654272/using-document-id-name-to-query-with-where-operators
+def get_previous_words(db, lastDate):
+    doc_ref = db.collection(u'word2').where(
+        u'exactTimeStamp', u'<=', lastDate).stream()
+    return [item.to_dict()['solution'] for item in doc_ref]
 
 
 if __name__ == '__main__':
@@ -31,15 +40,37 @@ if __name__ == '__main__':
 
     db = firestore.client()
 
-    words = get_data()
+    words_original = list(set(get_data()))
+    # The date where everything started. Do not change this so the solutionIndex has corrent numbers.
     actual_date = datetime.datetime(2022, 1, 13, 18, 0)
+
+    # first item where writing is alowed, give it to two days from today
+    min_date = datetime.datetime.now() + datetime.timedelta(days=2)
+    already_existing_words = get_previous_words(
+        db, min_date+datetime.timedelta(days=1))
+    print("Already existing words: ", already_existing_words)
+
+    print("Original words: ", words_original)
+    words = list(
+        filter(lambda x: x not in already_existing_words, words_original))
+    print("Words after removal existing: ", words)
+    print("Updating words since ", min_date)
+
+    random.shuffle(words)
+    random.shuffle(words)
+    random.shuffle(words)
+    random.shuffle(words)
+    words = words[0:30]
 
     for id, word in enumerate(words):
         actual_date = actual_date + datetime.timedelta(days=1)
+        if (actual_date < min_date):
+            continue
+
         print(actual_date, word)
         word = words[id]
         actual_date_str = actual_date.strftime('%Y-%m-%d')
-        doc_ref = db.collection(u'word').document(actual_date_str)
+        doc_ref = db.collection(u'word2').document(actual_date_str)
         doc_ref.set({
             'exactTimeStamp': actual_date,
             'solution': word,
--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,9 +1,83 @@
+/* eslint-disable object-curly-spacing */
+// https://firebase.google.com/docs/functions/typescript
+// good example: https://blog.logrocket.com/rest-api-firebase-cloud-functions-typescript-firestore/
+import * as admin from "firebase-admin";
 import * as functions from "firebase-functions";
 
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+admin.initializeApp();
+
+const db = admin.firestore();
+
+export const pad = (numv: number, size: number): string => {
+  let num = numv.toString();
+  while (num.length < size) num = "0" + num;
+  return num;
+};
+
+// returns like 2022-01-19
+export const dateToStr = (date: Date): string => {
+  return (
+    "" +
+    date.getFullYear() +
+    "-" +
+    pad(date.getMonth() + 1, 2) +
+    "-" +
+    pad(date.getDate(), 2)
+  );
+};
+
+// https://firebase.google.com/docs/functions/schedule-functions
+// https://cloud.google.com/appengine/docs/standard/python/config/cronref
+exports.unlockNewWord = functions.pubsub
+  .schedule("every day 08:00")
+  .timeZone("Europe/Prague")
+  .onRun(async (context) => {
+    try {
+      const now = new Date();
+      const actualDateStr = dateToStr(now);
+      functions.logger.info(
+        "Current date and time: ",
+        now.toString(),
+        ". Going to open word for: ",
+        actualDateStr +
+          ". Actual hour " +
+          now.getHours() +
+          " utc hour " +
+          now.getUTCHours()
+      );
+
+      const entry = db.collection("word").doc(actualDateStr);
+      const currentData = (await entry.get()).data() || {};
+      if (currentData && currentData.solution && currentData.solutionIndex) {
+        currentData.locked = false;
+        const retVal = await entry.set(currentData);
+        functions.logger.info(
+          "Successfully changed. Result value: " + retVal.toString()
+        );
+      } else {
+        functions.logger.error(
+          "Word item not found in database " + actualDateStr
+        );
+      }
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    } catch (error) {
+      functions.logger.error("Exception during processing: ", error);
+    }
+  });
+
+// TODO
+
+// It looks like you're using the development build of the Firebase JS SDK.
+// When deploying Firebase apps to production, it is advisable to only import
+// the individual SDK components you intend to use.
+
+// For the module builds, these are available in the following manner
+// (replace <PACKAGE> with the name of a component - i.e. auth, database, etc):
+
+// CommonJS Modules:
+// const firebase = require('firebase/app');
+// require('firebase/<PACKAGE>');
+
+// ES Modules:
+// import firebase from 'firebase/app';
+// import 'firebase/<PACKAGE>';
