4dd8dd76e10e1e1de4d7996a6f2037cc21df4f5e
Tonda Kmoch <tonda.kmoch@gmail.com>
2022-01-31 21:26:16 +0100

    theme

--- src/components/grid/CurrentRow.tsx
+++ src/components/grid/CurrentRow.tsx
@@ -4,6 +4,8 @@ type Props = {
   guess: string;
 };
 
+// ...
+
 export const CurrentRow = ({ guess }: Props) => {
   const splitGuess = guess.split("");
   const emptyCells = Array.from(Array(5 - splitGuess.length));
